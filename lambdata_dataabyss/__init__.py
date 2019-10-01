89104376bcbc7dd5c9d9218a146cae1d2b68fedd
dataabyss <dataabyss@users.noreply.github.com>
2019-09-30 21:34:14 -0700

    added some proof of concept utility functions

---
+++ lambdata_dataabyss/__init__.py
@@ -0,0 +1,11 @@
+"""
+utility functions for working with DataFrames
+"""
+
+import pandas as pd
+import numpy as np
+
+#sample code
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
